af3afb7b3ae616f7d9f2b6d3ca5c3369bfb6f260
Nithin SJ <thenithinraj@gmail.com>
2022-09-20 10:58:26 +0530

    Time: 35 ms (45.10%), Space: 19.6 MB (59.67%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int curr_majority_candidate = nums[0];
+        
+        int majority_freq = 0;
+        
+        for(int element: nums) {
+            if(element == curr_majority_candidate) {
+                majority_freq++;
+            }
+            else {
+                majority_freq--;
+                if(not majority_freq) {
+                    curr_majority_candidate = element;
+                    majority_freq++;
+                }
+            }
+        }
+        
+        return curr_majority_candidate;
+        
+    }
+};
