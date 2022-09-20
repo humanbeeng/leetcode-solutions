abf5b0912ba4046727ef1ac34591baaf3c21b2bd
Nithin SJ <thenithinraj@gmail.com>
2022-09-20 10:54:57 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (23.64%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n_elements = nums.size();
-        int left = 0, right = n_elements - 1;
+        int one_placeholder = 0, two_placeholder = n_elements - 1;
         
-        int p = left;
+        int p = one_placeholder;
         
-        while(p <= right) {
+        while(p <= two_placeholder) {
             
             if(nums[p] == 1) {
                 p++;
             }
             else if(nums[p] == 0) {
-                swap(nums[p++], nums[left++]);
+                swap(nums[p++], nums[one_placeholder++]);
             }
             else if(nums[p] == 2) {
-                swap(nums[p], nums[right--]);
+                swap(nums[p], nums[two_placeholder--]);
             }
             
         }
