a9c95f308c3250203777864eab25499204182164
Nithin SJ <thenithinraj@gmail.com>
2022-09-23 22:08:05 +0530

    Time: 382 ms (5.01%), Space: 48.1 MB (5.30%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    static auto Comp(const vector<int> a, const vector<int> b) -> bool {
+        return (a[0] < b[0]);
+    }
+    
+    auto merge(vector<vector<int>>& intervals) -> vector<vector<int>>{
+        vector<vector<int>> result;
+        int n_intervals = intervals.size();
+        
+        
+        sort(intervals.begin(), intervals.end(), Comp);
+        
+        for(auto interval: intervals) {
+            if(result.empty()) {
+                result.push_back(interval);
+                continue;
+            }
+            
+            if(result.back()[1] >= interval[0]) {
+                result.back()[0] = min(result.back()[0], interval[0]);
+                result.back()[1] = max(result.back()[1], interval[1]);
+            } else {
+                result.push_back(interval);
+            }
+        }
+        
+        return result;
+    }
+};
