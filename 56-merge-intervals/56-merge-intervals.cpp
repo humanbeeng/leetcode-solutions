1dfccc23f429ec6448387fe2d711ef59659a1bab
Nithin SJ <thenithinraj@gmail.com>
2022-09-24 10:21:04 +0530

    Time: 50 ms (74.46%), Space: 20 MB (32.53%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,27 +5,48 @@ public:
         return (a[0] < b[0]);
     }
     
-    auto merge(vector<vector<int>>& intervals) -> vector<vector<int>>{
-        vector<vector<int>> result;
-        int n_intervals = intervals.size();
+//     auto merge(vector<vector<int>>& intervals) -> vector<vector<int>>{
+//         vector<vector<int>> result;
+//         int n_intervals = intervals.size();
         
         
-        sort(intervals.begin(), intervals.end());
+//         sort(intervals.begin(), intervals.end());
         
-        for(auto interval: intervals) {
-            if(result.empty()) {
-                result.push_back(interval);
-                continue;
-            }
+//         for(auto interval: intervals) {
+//             if(result.empty()) {
+//                 result.push_back(interval);
+//                 continue;
+//             }
             
-            if(result.back()[1] >= interval[0]) {
-                result.back()[0] = min(result.back()[0], interval[0]);
-                result.back()[1] = max(result.back()[1], interval[1]);
-            } else {
-                result.push_back(interval);
-            }
-        }
+//             if(result.back()[1] >= interval[0]) {
+//                 result.back()[0] = min(result.back()[0], interval[0]);
+//                 result.back()[1] = max(result.back()[1], interval[1]);
+//             } else {
+//                 result.push_back(interval);
+//             }
+//         }
         
-        return result;
+//         return result;
+//     }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 0){
+            return intervals;
+        }
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        vector<int> element = intervals[0];
+        for(auto it: intervals){
+            
+            if(element[1] >= it[0] ){
+                element[1] = max(element[1], it[1]);
+            }
+            else{
+                res.push_back(element);
+                element = it;
+            } 
+        }
+        res.push_back(element);
+        return res;
+        
     }
 };
