41f09189c45c210b8d21720ac7415b68596a9e2c
Nithin SJ <thenithinraj@gmail.com>
2022-09-26 10:01:36 +0530

    Time: 38 ms (7.83%), Space: 15.9 MB (18.13%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,68 +1,67 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int c1 = 0;
-        int c2 = 0;
         
-        int f1 = 0, f2 = 0;
+        vector<int> majority_elements;
         
         int n_elements = nums.size();
         
-        vector<int> result;
-        
         if(n_elements == 1) {
-            result.push_back(nums[0]);
-            return result;
-        } 
+            majority_elements.push_back(nums[0]);
+            return majority_elements;
+        }
         
-        for(int i = 0; i < n_elements; i++ ){
+        int candidate_1 = nums[0], candidate_2 = nums[1], freq_1 = 0, freq_2 = 0;
+        
+        for(int i = 0; i < n_elements; i++) {
+            
             int curr_element = nums[i];
             
-            if(curr_element == c1) {
-                f1++;
-            } else if(curr_element == c2) {
-                f2++;
+            if(curr_element == candidate_1) {
+                freq_1++;
+            } 
+            
+            else if(curr_element == candidate_2) {
+                freq_2++;
             }
             
-            else if(f1 == 0) {
-                c1 = curr_element;
-                f1++;
+            else if(freq_1 == 0) {
+                candidate_1 = curr_element;
+                freq_1++;
             }
-            else if(f2 == 0) {
-                c2 = curr_element;
-                f2++;
+            
+            else if(freq_2 == 0) {
+                candidate_2 = curr_element;
+                freq_2++;
             }
             
             else {
-                f1--;
-                f2--;
+                freq_1--; freq_2--;
             }
-            
             
         }
         
-        f1 = 0; f2 = 0;
+        freq_1 = 0; freq_2 = 0;
         
         for(int element: nums) {
-            if(element == c1) {
-                f1++;
-            } else if(element == c2) {
-                f2++;
-            }
+            if(element == candidate_1) freq_1++;
+            else if(element == candidate_2) freq_2++;
+        }
+
+        
+        int majority_criteria = floor(n_elements / 3);
+        
+        if(freq_1 > majority_criteria) {
+            majority_elements.push_back(candidate_1);
         }
         
-        
-        int majority_criteria = n_elements / 3;
-        
-        if(f1 > majority_criteria) {
-            result.push_back(c1);
+        if(freq_2 > majority_criteria) {
+            majority_elements.push_back(candidate_2);
         }
         
-        if(f2 > majority_criteria) {
-            result.push_back(c2);
-        }
+        return majority_elements;
         
-        return result;
+        
         
     }
 };
