4d7e67d6578f46cabfc2b4b62e493f8fb206a3e8
Nithin SJ <thenithinraj@gmail.com>
2022-10-14 10:45:07 +0530

    Time: 4 ms (76.16%), Space: 13.8 MB (46.59%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    const char closed_parentheses = ')';
+    const char opened_parentheses = '(';
+
+    void helper(vector<string> &result, string parens, int opened_remain, int closed_remain) {
+        if(opened_remain == 0 and closed_remain == 0) {
+            // there are no more parentheses to add
+            result.push_back(parens);
+            return;
+        }
+
+        if(opened_remain > 0) {
+            helper(result, parens + opened_parentheses, opened_remain - 1, closed_remain);
+        }
+
+        if(closed_remain > opened_remain) {
+            helper(result, parens + closed_parentheses, opened_remain, closed_remain - 1);
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+       vector<string> result;
+       helper(result, "", n, n); 
+       return result;
+    }
+};
+// @lc code=end
+
