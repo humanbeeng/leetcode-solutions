ad867d35c194b94e618ac0eda6ab8aaffd26c4fb
Nithin SJ <thenithinraj@gmail.com>
2022-10-14 20:13:26 +0530

    Time: 4 ms (76.16%), Space: 15.8 MB (18.86%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,23 +2,34 @@ class Solution {
 public:
     const char closed_parentheses = ')';
     const char opened_parentheses = '(';
-
-    void helper(vector<string> &result, string parens, int opened_remain, int closed_remain) {
-        if(opened_remain == 0 and closed_remain == 0) {
-            // there are no more parentheses to add
+    
+    void helper(vector<string> &result, string parens, int opened_left, int closed_left) {
+        if(opened_left == 0 and closed_left == 0) {
+            // No more generation is possible
             result.push_back(parens);
             return;
         }
-
-        if(opened_remain > 0) {
-            helper(result, parens + opened_parentheses, opened_remain - 1, closed_remain);
+        if(opened_left > 0) {
+            parens.push_back(opened_parentheses);
+            helper(result, parens, opened_left - 1, closed_left);
+            parens.pop_back();
+            
+            //or
+            // helper(result, parens + opened_parentheses, opened_left - 1, closed_left)
         }
-
-        if(closed_remain > opened_remain) {
-            helper(result, parens + closed_parentheses, opened_remain, closed_remain - 1);
+        if(closed_left > opened_left) {
+            // as soon as there are more opened_parentheses than closed, we attempt to put 
+            // closed_parentheses to balance it out
+            
+            parens.push_back(closed_parentheses);
+            helper(result, parens, opened_left, closed_left - 1);
+            parens.pop_back();
+            
+            // or 
+            // helper(result, parens + closed_parentheses, opened_left, closed_left - 1)
         }
-
     }
+    
     vector<string> generateParenthesis(int n) {
        vector<string> result;
        helper(result, "", n, n); 
