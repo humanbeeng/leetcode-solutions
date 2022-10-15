96bd145736835d268f04a4bc52e928682b625958
Nithin SJ <thenithinraj@gmail.com>
2022-10-15 15:34:13 +0530

    Time: 14 ms (54.99%), Space: 10.8 MB (91.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -9,21 +9,22 @@ public:
             return;
         } 
         
-            if(candidates[i] <= target) {
+        if(candidates[i] <= target) {
                 
-//          Choose to pick, but, not moving i, since we can pick curr_element unlimited times
-                combination.push_back(candidates[i]);
+//      Choose to pick, but, not moving i, since we can pick curr_element unlimited times
+            combination.push_back(candidates[i]);
                 
-                helper(result, combination, candidates, target - candidates[i], i);
+            helper(result, combination, candidates, target - candidates[i], i);
                 
-                combination.pop_back();
+            combination.pop_back();
                 
-//           Choose not to pick
-                helper(result, combination, candidates, target, i + 1);
+//          Choose not to pick
+            helper(result, combination, candidates, target, i + 1);
                 
-            } else {
-                helper(result, combination, candidates, target, i + 1);
-            }
+        } else {
+//             Move on
+            helper(result, combination, candidates, target, i + 1);
+        }
         
         
     }
