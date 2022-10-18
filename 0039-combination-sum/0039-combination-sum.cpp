ca4a4c1de28c866ad5307ed61b34e0705847ca6f
Nithin SJ <thenithinraj@gmail.com>
2022-10-18 10:57:40 +0530

    Time: 7 ms (87.23%), Space: 17.2 MB (34.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    
-    void helper(vector<vector<int>> &result, vector<int> &combination, vector<int> &candidates, int target, int i) {
-        
-        if(i >= candidates.size()) { 
-            // end state, ie -> all nodes are processed and we are at leaves of 
-            // recursion tree
-            
-            if(target == 0)
+
+    void helper(vector<int> &candidates, vector<vector<int>> &result, vector<int> &combination, int target, int i) {
+        if(i >= candidates.size()) {
+            if(target == 0) {
                 result.push_back(combination);
+            }
             return;
-        } 
-        
-        if(candidates[i] <= target) {
-                
-//      Choose to pick, but, not moving i, since we can pick curr_element unlimited times
-            combination.push_back(candidates[i]);
-                
-            helper(result, combination, candidates, target - candidates[i], i);
-                
-            combination.pop_back();
-                
-//          Choose not to pick
-            helper(result, combination, candidates, target, i + 1);
-                
-        } else {
-//             Move on
-            helper(result, combination, candidates, target, i + 1);
         }
-        
-        
+
+        int curr_element = candidates[i];
+
+        if(curr_element <= target) {
+            // Choice 1: Pick. But since we can keep on picking the same element any n times, we stay here and
+            // dont move to next element
+            combination.push_back(curr_element);
+            helper(candidates, result, combination, target - curr_element, i);
+            // Revert the state to backtrack
+            combination.pop_back();
+
+            // Choice 2: Don't pick
+            helper(candidates, result, combination, target, i + 1);
+
+        } else {
+            // Not picking
+            helper(candidates, result, combination, target, i + 1);
+        }
     }
-    
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> combination;
-        
-        helper(result, combination, candidates, target, 0);
-        
-        return result;
+       vector<vector<int>> result;
+       vector<int> combination;
+
+       helper(candidates, result, combination, target, 0);
+
+       return result;
+
     }
 };
