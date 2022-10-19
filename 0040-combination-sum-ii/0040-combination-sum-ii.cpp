80852d2a989f943797435cabae8d2dc097dd48af
Nithin SJ <thenithinraj@gmail.com>
2022-10-19 23:56:01 +0530

    Time: 19 ms (24.85%), Space: 10.6 MB (73.42%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     void helper(vector<int> &candidates, vector<vector<int>> &result, vector<int> &combination, int target, int start) {
         
-        if(target == 0 or start >= candidates.size()){
-            if(target == 0)
-                result.push_back(combination);
+        if(target < 0) return;
+
+        if(target == 0) {
+            result.push_back(combination);
             return;
-        } 
-        
-        for(int i = start; i < candidates.size(); i++) {
-            if(candidates[i] > target) {
-                break;
-            }
-            if(i != start and candidates[i] == candidates[i - 1])
-                continue;
-            
-            if(candidates[i] <= target) {
-                combination.push_back(candidates[i]);
-                helper(candidates, result, combination, target - candidates[i], i + 1);
-                combination.pop_back();
-            } else {
-                helper(candidates, result, combination, target, i + 1);
-            }
-            
         }
-                
-            
-            
+
+        if(start >= candidates.size()) return;
+
+        for(int i = start; i < candidates.size(); i++) {
+            int curr_element = candidates[i];
+
+            if(i > start and curr_element == candidates[i - 1]) continue;
+
+            combination.push_back(curr_element);
+            helper(candidates, result, combination, target - curr_element, i + 1);
+            combination.pop_back();
+
+        }    
         
     }
     
