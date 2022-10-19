24e025b1a0bc7fe05912bb68532e6f9060a51619
Nithin SJ <thenithinraj@gmail.com>
2022-10-20 00:07:45 +0530

    Time: 4 ms (69.26%), Space: 7.6 MB (70.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void generateSubsets(vector<int> &nums, vector<vector<int>> &result, vector<int> &subset, int start) {
+    void helper(vector<int> &nums, vector<vector<int>> &result, vector<int> &combination, int start) {
         
-        result.push_back(subset);
+        result.push_back(combination);
         
         for(int i = start; i < nums.size(); i++) {
-            if(i > start and nums[i] == nums[i - 1])
-                continue;
+            int curr_element = nums[i];
+            if(i > start and nums[i - 1] == curr_element) continue;
             
-            subset.push_back(nums[i]);
-            generateSubsets(nums, result, subset, i + 1);
-            subset.pop_back();
+            combination.push_back(curr_element);
+            helper(nums, result, combination, i + 1);
+            combination.pop_back();
         }
         
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> subset;
-        
         sort(nums.begin(), nums.end());
         
-        generateSubsets(nums, result, subset, 0);
+        vector<vector<int>> result;
+        vector<int> combination;
+        
+        helper(nums, result, combination, 0);
         
         return result;
     }
