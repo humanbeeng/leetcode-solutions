d3af784708834622dc8abf91289d7eda20700f16
Nithin SJ <thenithinraj@gmail.com>
2022-10-28 10:23:36 +0530

    Time: 88 ms (29.13%), Space: 24 MB (63.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n_nums = nums.size();
-        nums.resize(n_nums * 2);
-        
-        vector<int> ngr;
         stack<int> s;
+        int n_elements = nums.size();
+        vector<int> nge(n_elements, -1);
         
-        for(int i = n_nums; i < (2 * n_nums); i++) {
-            // from the end of nums
-            nums[i] = nums[i - n_nums];
+        for(int i = 0; i < (2 * n_elements); i++) {
+            int idx = i % n_elements;
+            int curr_element = nums[idx];
             
-        }
-        
-        for(int i = n_nums; i >= 0; i--) {
-            s.push(nums[i]);
-        }
-        
-        for(int i = n_nums - 1; i >= 0; i--) {
-            int curr_element = nums[i];
-            
-            if(s.empty()) {
-                ngr.push_back(-1);
-            } else {
-                if(s.top() > curr_element) {
-                    ngr.push_back(s.top());
-                } else {
-                    while(not s.empty() and s.top() <= curr_element) {
-                    s.pop();
-                    }
-            
-                    if(s.empty()) {
-                        ngr.push_back(-1);
-                    }
-                    else if(s.top() > curr_element) {
-                        ngr.push_back(s.top());
-                    }
-                }
+            // curr element is tested as nge for all the elements in stack. 
+            // ie, prev elements
+            while(not s.empty() and curr_element > nums[s.top()]) {
+                // if so, then we update their nge as curr_element in result arr;
+                nge[s.top()] = curr_element;
                 
+                // Since we have found nge of this top element, it is processed, 
+                // hence popped out.
+                s.pop(); 
             }
-            
-            s.push(curr_element);
+            // Push the curr_element to be be processed, ie, to find nge for it. 
+            s.push(idx);
         }
-        reverse(ngr.begin(), ngr.end());
         
-        return ngr;
         
+        return nge;
     }
 };
