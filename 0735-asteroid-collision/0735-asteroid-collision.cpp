54c800e5451ceb34a16443cb4aa72760c2c78698
Nithin SJ <thenithinraj@gmail.com>
2022-10-31 20:30:40 +0530

    Time: 7 ms (99.72%), Space: 17.4 MB (84.12%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -13,9 +13,16 @@ public:
                 while(not s.empty() and s.back() > 0 and abs(curr_size) > s.back()) {    
                     s.pop_back();
                 }
-                
-                if(s.empty() or s.back() < 0) s.push_back(curr_size);
-                else if(s.back() == abs(curr_size)) s.pop_back();
+//                 Destruction stopped when either there's no asteroid left to destroy or 
+//                 we have encountered asteroid of same direction (-, left). In that case, 
+//                 curr_asteroid will just remain in the state forever.
+//                 OR we have encountered asteroid of same size and opposite dir(right)
+                    
+                if(s.empty() or s.back() < 0) 
+                    s.push_back(curr_size);
+                else if(s.back() > 0 and s.back() == abs(curr_size)) 
+                    s.pop_back();
+
             }
             
         }
