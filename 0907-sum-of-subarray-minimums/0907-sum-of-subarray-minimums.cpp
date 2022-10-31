2f0ab453f3a198b9f730fa11017ff7a8389cad74
Nithin SJ <thenithinraj@gmail.com>
2022-10-31 11:01:25 +0530

    Time: 112 ms (90.46%), Space: 42.8 MB (60.41%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    
+    void calculateNSL(vector<int> &arr, int n_elements, vector<int> &nsl) {
+        stack<int> s;
+        
+        for(int i = n_elements - 1; i >= 0; i--) {
+            int curr_element = arr[i];
+            
+            while(not s.empty() and curr_element < arr[s.top()]) {
+                nsl[s.top()] = i;
+                s.pop();
+            }
+            
+            s.push(i);
+            
+        }
+    }
+    
+    void calculateNSR(vector<int> &arr, int n_elements, vector<int> &nsr) {
+        stack<int> s;
+        
+        for(int i = 0; i < n_elements; i++) {
+            int curr_element = arr[i];
+            
+            while(not s.empty() and curr_element <= arr[s.top()]) {
+                nsr[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+    }
+    
+
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        int n_elements = arr.size();
+        int mod = 1e9 + 7, sum_subarray_mins = 0;
+        
+        vector<int> nsl(n_elements, -1);
+        vector<int> nsr(n_elements, n_elements);
+        
+      
+        calculateNSL(arr, n_elements, nsl);
+        calculateNSR(arr, n_elements, nsr);
+        
+        
+        for(int i = 0; i < n_elements; i++) {
+            int curr_element = arr[i];
+            
+            int m = i - nsl[i];
+            int n = nsr[i] - i;
+            
+            long long product = (m * n) % mod;
+            product = (product * curr_element) % mod;
+            
+            sum_subarray_mins = (sum_subarray_mins + product) % mod;
+        }
+        
+        return sum_subarray_mins;
+    }
+};
